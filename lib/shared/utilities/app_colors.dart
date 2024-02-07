446719336f8183db141f48e0da6866b41de75f93
habyaad <adeomotoshan@gmail.com>
2024-02-07 12:53:50 +0100

    feat: create app color palette

---
+++ lib/shared/utilities/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // karot colors
+  static const Color primary500 = Color(0xFF2453AD);
+  static const Color textBlack = Color(0xFF1B1B28);
+  static const Color appGrey = Color(0xFFD9D9D9);
+  static const Color appGreyDark = Color(0xFFB1ADAD);
+  static const Color appLight50 = Color(0xFFB3B6CB);
+
+}
