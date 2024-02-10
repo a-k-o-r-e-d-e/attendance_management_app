5b4425f55a26bcb27851827b214bfadaf72a6937
habyaad <adeomotoshan@gmail.com>
2024-02-10 13:17:31 +0100

    remove: remove duplicate color from palette

--- lib/shared/utilities/app_colors.dart
+++ lib/shared/utilities/app_colors.dart
@@ -5,10 +5,10 @@ class AppColors {
 
   // karot colors
   static const Color primary500 = Color(0xFF2453AD);
-  static const Color textBlack = Color(0xFF1B1B28);
   static const Color appGrey = Color(0xFFD9D9D9);
   static const Color appGreyDark = Color(0xFFB1ADAD);
   static const Color appLight50 = Color(0xFFB3B6CB);
+  static const Color appLight30 = Color(0xFFE8E9F0);
   static const Color appDark700 = Color(0xFF1B1B28);
   static const Color placeHolder = Color(0xFFB3B6CB);
   static const Color medium200 = Color(0xFF868EA3);
