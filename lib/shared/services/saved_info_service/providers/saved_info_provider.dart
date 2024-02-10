5ba9a345ac0fa9ff805344b09ee8c10b71c17c81
habyaad <adeomotoshan@gmail.com>
2024-02-10 13:15:48 +0100

    feat: create saved info provider

---
+++ lib/shared/services/saved_info_service/providers/saved_info_provider.dart
@@ -0,0 +1,17 @@
+import 'package:attendance_management_app/shared/services/saved_info_service/data/saved_info_impl.dart';
+import 'package:attendance_management_app/shared/services/saved_info_service/domain/repository/saved_info_repo.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+
+part 'saved_info_provider.g.dart';
+
+@riverpod
+class SavedInfoServiceMethods extends _$SavedInfoServiceMethods {
+  @override
+  SavedInfoService build() {
+    const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+    return SavedInfoImpl(secureStorage);
+  }
+// Add methods to mutate the state
+}
