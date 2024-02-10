ad836e53df7e936dfd930526c2055f0a0cc73e51
habyaad <adeomotoshan@gmail.com>
2024-02-10 13:22:17 +0100

    feat: create constant app strings

---
+++ lib/shared/utilities/app_strings.dart
@@ -0,0 +1,4 @@
+const String AUTH_TOKEN_KEY = "token";
+const String APP_FIRST_RUN_KEY = 'check-first-run';
+const String USER_JSON_KEY = "user-json";
+const String EXPIRATION_TIME_KEY = "token-expire";
