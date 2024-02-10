0b9cfcdd913b75949700b7333f453c025931dbf5
habyaad <adeomotoshan@gmail.com>
2024-02-10 13:28:26 +0100

    feat: create upcoming class model

---
+++ lib/features/home/domain/model/upcoming_class_model.dart
@@ -0,0 +1,12 @@
+class UpcomingClassModel {
+  final String? courseTitle;
+  final int? studentEnrolled;
+  final DateTime? startDate;
+  final String? coverImage;
+
+  UpcomingClassModel(
+      {this.courseTitle,
+      this.studentEnrolled,
+      this.startDate,
+      this.coverImage});
+}
