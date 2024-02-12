03429660c992d46211a5a191e3dd828ab8077897
habyaad <adeomotoshan@gmail.com>
2024-02-12 09:39:54 +0100

    feat: add new color to palette

--- lib/shared/utilities/app_colors.dart
+++ lib/shared/utilities/app_colors.dart
@@ -9,6 +9,7 @@ class AppColors {
   static const Color appGreyDark = Color(0xFFB1ADAD);
   static const Color appLight50 = Color(0xFFB3B6CB);
   static const Color appLight30 = Color(0xFFE8E9F0);
+  static const Color appLight20 = Color(0xFFF2F2F8);
   static const Color appDark700 = Color(0xFF1B1B28);
   static const Color placeHolder = Color(0xFFB3B6CB);
   static const Color medium200 = Color(0xFF868EA3);
