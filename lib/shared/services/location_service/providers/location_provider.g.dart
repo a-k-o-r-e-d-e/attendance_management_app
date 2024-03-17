6eaad05bb306a3860bd8d83c0ee0d8e184914317
habyaad <adeomotoshan@gmail.com>
2024-03-17 18:04:23 +0100

    feat: create location provider

---
+++ lib/shared/services/location_service/providers/location_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'location_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$locationServiceMethodsHash() =>
+    r'5a24bf7db61939d362468e215f21bf4ec52ad443';
+
+/// See also [LocationServiceMethods].
+@ProviderFor(LocationServiceMethods)
+final locationServiceMethodsProvider = AutoDisposeNotifierProvider<
+    LocationServiceMethods, LocationService>.internal(
+  LocationServiceMethods.new,
+  name: r'locationServiceMethodsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$locationServiceMethodsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LocationServiceMethods = AutoDisposeNotifier<LocationService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
