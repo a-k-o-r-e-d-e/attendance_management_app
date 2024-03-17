94a13a36adf8638c2dd2c1403b197550625b0944
habyaad <adeomotoshan@gmail.com>
2024-03-17 17:48:18 +0100

    feat: create app string class

--- lib/shared/utilities/app_strings.dart
+++ lib/shared/utilities/app_strings.dart
@@ -1,4 +1,9 @@
-const String AUTH_TOKEN_KEY = "token";
-const String APP_FIRST_RUN_KEY = 'check-first-run';
-const String USER_JSON_KEY = "user-json";
-const String EXPIRATION_TIME_KEY = "token-expire";
+class AppStrings {
+  AppStrings._();
+
+  static const String AUTH_TOKEN_KEY = "token";
+  static const String APP_FIRST_RUN_KEY = 'check-first-run';
+  static const String USER_JSON_KEY = "user-json";
+  static const String EXPIRATION_TIME_KEY = "token-expire";
+  static const String ENABLE_GEOFENCING = "enable-geofencing";
+}
