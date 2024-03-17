ade15134d5b9e3c5badb52379ccaaf42809c6184
habyaad <adeomotoshan@gmail.com>
2024-03-17 17:46:59 +0100

    feat: register new routes

--- lib/shared/routes/app_route.dart
+++ lib/shared/routes/app_route.dart
@@ -1,27 +1,49 @@
 import 'package:attendance_management_app/features/authentication/presentation/screens/create_account_screen.dart';
 import 'package:attendance_management_app/features/authentication/presentation/screens/login_screen.dart';
 import 'package:attendance_management_app/features/authentication/presentation/screens/select_user_type_screen.dart';
+import 'package:attendance_management_app/features/class/presentation/screens/attendance_screen.dart';
 import 'package:attendance_management_app/features/class/presentation/screens/class_screen.dart';
 import 'package:attendance_management_app/features/class/presentation/screens/create_class_screen.dart';
+import 'package:attendance_management_app/features/class/presentation/screens/single_class_screen.dart';
+import 'package:attendance_management_app/features/class/presentation/screens/start_class_screen.dart';
+import 'package:attendance_management_app/features/course/domain/models/course_model.dart';
+import 'package:attendance_management_app/features/course/presentation/screens/course_screen.dart';
+import 'package:attendance_management_app/features/course/presentation/screens/create_course_screen.dart';
+import 'package:attendance_management_app/features/course/presentation/screens/single_course_screen.dart';
+import 'package:attendance_management_app/features/course/presentation/screens/student_info.dart';
+import 'package:attendance_management_app/features/course/presentation/screens/student_list.dart';
 import 'package:attendance_management_app/features/home/presentation/screens/home_screen.dart';
 import 'package:attendance_management_app/features/onboarding/presentation/screens/onboarding_screen.dart';
+import 'package:attendance_management_app/features/profile/presentation/screens/edit_profile_screen.dart';
+import 'package:attendance_management_app/features/profile/presentation/screens/profile_screen.dart';
+import 'package:attendance_management_app/features/profile/presentation/screens/settings_screen.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+
 part 'app_route.gr.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Screen,Route')
 class AppRouter extends _$AppRouter {
-
   @override
   List<AutoRoute> get routes => [
-    /// routes go here
-    AutoRoute(page: OnboardingRoute.page, initial: true, path: "/"),
-    AutoRoute(page: SelectUserTypeRoute.page, path: "/select-user-type"),
-    AutoRoute(page: CreateAccountRoute.page, path: "/create-account"),
-    AutoRoute(page: LoginRoute.page, path: "/login"),
-    AutoRoute(page: HomeRoute.page, path: "/home"),
-    AutoRoute(page: ClassRoute.page, path: "/class"),
-    AutoRoute(page: CreateClassRoute.page, path: "/create-class"),
-
+        /// routes go here
+        AutoRoute(page: OnboardingRoute.page, initial: true, path: "/"),
+        AutoRoute(page: SelectUserTypeRoute.page, path: "/select-user-type"),
+        AutoRoute(page: CreateAccountRoute.page, path: "/create-account"),
+        AutoRoute(page: LoginRoute.page, path: "/login"),
+        AutoRoute(page: HomeRoute.page, path: "/home"),
+        AutoRoute(page: ClassRoute.page, path: "/class"),
+        AutoRoute(page: CreateClassRoute.page, path: "/create-class"),
+        AutoRoute(page: CreateCourseRoute.page, path: "/create-course"),
+        AutoRoute(page: CourseRoute.page, path: "/course"),
+        AutoRoute(page: SingleCourseRoute.page, path: "/single-course"),
+        AutoRoute(page: SingleClassRoute.page, path: "/single-class"),
+        AutoRoute(page: StudentListRoute.page, path: "/student-list"),
+        AutoRoute(page: StudentInfoRoute.page, path: "/student-info"),
+        AutoRoute(page: StartClassRoute.page, path: "/start-class"),
+        AutoRoute(page: AttendanceRoute.page, path: "/attendance"),
+        AutoRoute(page: ProfileRoute.page, path: "/profile"),
+        AutoRoute(page: EditProfileRoute.page, path: "/edit-profile"),
+        AutoRoute(page: SettingsRoute.page, path: "/settings"),
   ];
 }
--- lib/shared/routes/app_route.gr.dart
+++ lib/shared/routes/app_route.gr.dart
@@ -15,10 +15,22 @@ abstract class _$AppRouter extends RootStackRouter {
 
   @override
   final Map<String, PageFactory> pagesMap = {
+    AttendanceRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const AttendanceScreen(),
+      );
+    },
     ClassRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const ClassScreen(),
+      );
+    },
+    CourseRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CourseScreen(),
       );
     },
     CreateAccountRoute.name: (routeData) {
@@ -31,6 +43,18 @@ abstract class _$AppRouter extends RootStackRouter {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const CreateClassScreen(),
+      );
+    },
+    CreateCourseRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CreateCourseScreen(),
+      );
+    },
+    EditProfileRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const EditProfileScreen(),
       );
     },
     HomeRoute.name: (routeData) {
@@ -53,13 +77,82 @@ abstract class _$AppRouter extends RootStackRouter {
         child: OnboardingScreen(key: args.key),
       );
     },
+    ProfileRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const ProfileScreen(),
+      );
+    },
     SelectUserTypeRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const SelectUserTypeScreen(),
       );
     },
+    SettingsRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const SettingsScreen(),
+      );
+    },
+    SingleClassRoute.name: (routeData) {
+      final args = routeData.argsAs<SingleClassRouteArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: SingleClassScreen(
+          key: args.key,
+          course: args.course,
+        ),
+      );
+    },
+    SingleCourseRoute.name: (routeData) {
+      final args = routeData.argsAs<SingleCourseRouteArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: SingleCourseScreen(
+          key: args.key,
+          course: args.course,
+        ),
+      );
+    },
+    StartClassRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const StartClassScreen(),
+      );
+    },
+    StudentInfoRoute.name: (routeData) {
+      final args = routeData.argsAs<StudentInfoRouteArgs>(
+          orElse: () => const StudentInfoRouteArgs());
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: StudentInfoScreen(
+          key: args.key,
+          attendance: args.attendance,
+        ),
+      );
+    },
+    StudentListRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const StudentListScreen(),
+      );
+    },
   };
+}
+
+/// generated route for
+/// [AttendanceScreen]
+class AttendanceRoute extends PageRouteInfo<void> {
+  const AttendanceRoute({List<PageRouteInfo>? children})
+      : super(
+          AttendanceRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AttendanceRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
 }
 
 /// generated route for
@@ -72,6 +165,20 @@ class ClassRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'ClassRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [CourseScreen]
+class CourseRoute extends PageRouteInfo<void> {
+  const CourseRoute({List<PageRouteInfo>? children})
+      : super(
+          CourseRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CourseRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
@@ -100,6 +207,34 @@ class CreateClassRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'CreateClassRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [CreateCourseScreen]
+class CreateCourseRoute extends PageRouteInfo<void> {
+  const CreateCourseRoute({List<PageRouteInfo>? children})
+      : super(
+          CreateCourseRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CreateCourseRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [EditProfileScreen]
+class EditProfileRoute extends PageRouteInfo<void> {
+  const EditProfileRoute({List<PageRouteInfo>? children})
+      : super(
+          EditProfileRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'EditProfileRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
@@ -162,6 +297,20 @@ class OnboardingRouteArgs {
 }
 
 /// generated route for
+/// [ProfileScreen]
+class ProfileRoute extends PageRouteInfo<void> {
+  const ProfileRoute({List<PageRouteInfo>? children})
+      : super(
+          ProfileRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ProfileRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
 /// [SelectUserTypeScreen]
 class SelectUserTypeRoute extends PageRouteInfo<void> {
   const SelectUserTypeRoute({List<PageRouteInfo>? children})
@@ -171,6 +320,162 @@ class SelectUserTypeRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'SelectUserTypeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [SettingsScreen]
+class SettingsRoute extends PageRouteInfo<void> {
+  const SettingsRoute({List<PageRouteInfo>? children})
+      : super(
+          SettingsRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SettingsRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [SingleClassScreen]
+class SingleClassRoute extends PageRouteInfo<SingleClassRouteArgs> {
+  SingleClassRoute({
+    Key? key,
+    required CourseModel course,
+    List<PageRouteInfo>? children,
+  }) : super(
+          SingleClassRoute.name,
+          args: SingleClassRouteArgs(
+            key: key,
+            course: course,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'SingleClassRoute';
+
+  static const PageInfo<SingleClassRouteArgs> page =
+      PageInfo<SingleClassRouteArgs>(name);
+}
+
+class SingleClassRouteArgs {
+  const SingleClassRouteArgs({
+    this.key,
+    required this.course,
+  });
+
+  final Key? key;
+
+  final CourseModel course;
+
+  @override
+  String toString() {
+    return 'SingleClassRouteArgs{key: $key, course: $course}';
+  }
+}
+
+/// generated route for
+/// [SingleCourseScreen]
+class SingleCourseRoute extends PageRouteInfo<SingleCourseRouteArgs> {
+  SingleCourseRoute({
+    Key? key,
+    required CourseModel course,
+    List<PageRouteInfo>? children,
+  }) : super(
+          SingleCourseRoute.name,
+          args: SingleCourseRouteArgs(
+            key: key,
+            course: course,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'SingleCourseRoute';
+
+  static const PageInfo<SingleCourseRouteArgs> page =
+      PageInfo<SingleCourseRouteArgs>(name);
+}
+
+class SingleCourseRouteArgs {
+  const SingleCourseRouteArgs({
+    this.key,
+    required this.course,
+  });
+
+  final Key? key;
+
+  final CourseModel course;
+
+  @override
+  String toString() {
+    return 'SingleCourseRouteArgs{key: $key, course: $course}';
+  }
+}
+
+/// generated route for
+/// [StartClassScreen]
+class StartClassRoute extends PageRouteInfo<void> {
+  const StartClassRoute({List<PageRouteInfo>? children})
+      : super(
+          StartClassRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'StartClassRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [StudentInfoScreen]
+class StudentInfoRoute extends PageRouteInfo<StudentInfoRouteArgs> {
+  StudentInfoRoute({
+    Key? key,
+    bool? attendance = false,
+    List<PageRouteInfo>? children,
+  }) : super(
+          StudentInfoRoute.name,
+          args: StudentInfoRouteArgs(
+            key: key,
+            attendance: attendance,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'StudentInfoRoute';
+
+  static const PageInfo<StudentInfoRouteArgs> page =
+      PageInfo<StudentInfoRouteArgs>(name);
+}
+
+class StudentInfoRouteArgs {
+  const StudentInfoRouteArgs({
+    this.key,
+    this.attendance = false,
+  });
+
+  final Key? key;
+
+  final bool? attendance;
+
+  @override
+  String toString() {
+    return 'StudentInfoRouteArgs{key: $key, attendance: $attendance}';
+  }
+}
+
+/// generated route for
+/// [StudentListScreen]
+class StudentListRoute extends PageRouteInfo<void> {
+  const StudentListRoute({List<PageRouteInfo>? children})
+      : super(
+          StudentListRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'StudentListRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
