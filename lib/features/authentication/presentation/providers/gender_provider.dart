78d4f160a91e6da452aec7702353a5a6388266f2
habyaad <adeomotoshan@gmail.com>
2024-03-17 18:04:05 +0100

    feat: create gender provider

---
+++ lib/features/authentication/presentation/providers/gender_provider.dart
@@ -0,0 +1,19 @@
+import 'package:attendance_management_app/features/authentication/presentation/providers/states/user_type_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'gender_provider.g.dart';
+
+@riverpod
+class GenderType extends _$GenderType {
+
+  @override
+  GenderEnum build() {
+    return GenderEnum.none;
+  }
+
+  void setGender(GenderEnum type) {
+    state = type;
+  }
+
+// Add methods to mutate the state
+}
