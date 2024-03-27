fbdc8235dfcb9e49010863ed7cd04f541a30e2f7
habyaad <adeomotoshan@gmail.com>
2024-03-27 13:28:52 +0100

    feat: create profile repo provider

---
+++ lib/features/profile/data/repository/profile_repo_impl.dart
@@ -0,0 +1,25 @@
+import 'package:attendance_management_app/features/authentication/domain/models/user_model.dart';
+import 'package:attendance_management_app/features/authentication/presentation/providers/states/user_type_state.dart';
+import '../../domain/repository/profile_repo.dart';
+import '../data_source/profile_remote_data_source.dart';
+
+class ProfileRepositoryImpl extends ProfileRepository {
+  final ProfileDataSource dataSource;
+
+  ProfileRepositoryImpl(this.dataSource);
+
+  @override
+  Future<Profile> fetchMyProfile(UserType userType) {
+    return dataSource.fetchMyProfile(userType);
+  }
+
+  @override
+  Future<Profile> fetchProfileById(UserType userType, String id) {
+    return dataSource.fetchProfileById(userType, id);
+  }
+
+  @override
+  Future<Profile> updateProfile(UserType userType, Map<String, dynamic> userData) {
+    return dataSource.updateProfile(userType, userData);
+  }
+}
---
+++ lib/features/profile/domain/repository/profile_repo.dart
@@ -0,0 +1,12 @@
+import 'package:attendance_management_app/features/authentication/domain/models/user_model.dart';
+
+import '../../../authentication/presentation/providers/states/user_type_state.dart';
+
+abstract class ProfileRepository {
+  Future<Profile> fetchMyProfile(UserType userType);
+
+  Future<Profile> updateProfile(
+      UserType userType, Map<String, dynamic> userData);
+
+  Future<Profile> fetchProfileById(UserType userType, String id);
+}
