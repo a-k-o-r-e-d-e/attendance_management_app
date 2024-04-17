cf9158b17a9d5093a97c368be9b0600cbebba1fc
habyaad <adeomotoshan@gmail.com>
2024-04-17 09:02:49 +0100

    refactor: integrate widget with model

--- lib/features/course/presentation/widgets/class_card.dart
+++ lib/features/course/presentation/widgets/class_card.dart
@@ -1,11 +1,16 @@
+import 'package:attendance_management_app/features/class/domain/models/course_class_model.dart';
 import 'package:attendance_management_app/shared/utilities/app_colors.dart';
 import 'package:attendance_management_app/shared/utilities/size_utils.dart';
+import 'package:attendance_management_app/shared/utilities/string_utils.dart';
 import 'package:attendance_management_app/shared/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 class ClassCard extends StatelessWidget {
-  const ClassCard({super.key});
+  final CourseClassesModel classInstance;
+
+  const ClassCard({super.key, required this.classInstance});
 
   @override
   Widget build(BuildContext context) {
@@ -23,42 +28,47 @@ class ClassCard extends StatelessWidget {
             decoration: BoxDecoration(
                 color: Colors.grey, borderRadius: BorderRadius.circular(4)),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const CustomText(
-                title: "Overview of Computer Science",
-                size: 14,
-                weight: FontWeight.w600,
-              ),
-              Row(
-                children: [
-                  SvgPicture.asset(
-                    "assets/svgs/clock.svg",
-                    colorFilter: const ColorFilter.mode(
-                        AppColors.medium200, BlendMode.srcIn),
-                  ),
-                  horizontalSpace(8),
-                  const CustomText(
-                    title: "08 - 10AM",
-                    size: 14,
-                    weight: FontWeight.w500,
-                  ),
-                  horizontalSpace(16),
-                  SvgPicture.asset(
-                    "assets/svgs/calendar.svg",
-                    colorFilter: const ColorFilter.mode(
-                        AppColors.medium200, BlendMode.srcIn),
-                  ),
-                  horizontalSpace(8),
-                  const CustomText(
-                    title: "15/02",
-                    size: 14,
-                    weight: FontWeight.w500,
-                  ),
-                ],
-              )
-            ],
+          horizontalSpaceSmall,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomText(
+                  title: classInstance.title,
+                  size: 14,
+                  weight: FontWeight.w600,
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      "assets/svgs/clock.svg",
+                      colorFilter: const ColorFilter.mode(
+                          AppColors.medium200, BlendMode.srcIn),
+                    ),
+                    horizontalSpace(8),
+                    CustomText(
+                      title:
+                          "${StringUtils.formatTime(classInstance.startTime.toString())} - ${StringUtils.formatTime(classInstance.endTime.toString())}",
+                      size: 14,
+                      weight: FontWeight.w500,
+                    ),
+                    horizontalSpace(16),
+                    SvgPicture.asset(
+                      "assets/svgs/calendar.svg",
+                      colorFilter: const ColorFilter.mode(
+                          AppColors.medium200, BlendMode.srcIn),
+                    ),
+                    horizontalSpace(8),
+                    CustomText(
+                      title:
+                          DateFormat('dd/MM').format(classInstance.startDate!),
+                      size: 14,
+                      weight: FontWeight.w500,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
           const Icon(
             Icons.keyboard_arrow_right_rounded,
