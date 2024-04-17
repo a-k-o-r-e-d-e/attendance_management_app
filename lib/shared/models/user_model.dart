2e269835e77fb73f3ec2ad774491897daa66b3c7
habyaad <adeomotoshan@gmail.com>
2024-04-17 08:58:34 +0100

    chore: move files

--- lib/shared/models/user_model.dart
+++ lib/shared/models/user_model.dart
@@ -4,6 +4,8 @@
 
 import 'dart:convert';
 
+import 'institution_model.dart';
+
 UserAccount userAccountFromJson(String str) => UserAccount.fromJson(json.decode(str));
 
 String userAccountToJson(UserAccount data) => json.encode(data.toJson());
@@ -81,46 +83,6 @@ class Profile {
     "matric_no": matricNo,
     "user": user?.toJson(),
     "institution": institution?.toJson(),
-  };
-}
-
-class Institution {
-  final String? id;
-  final String? name;
-  final String? abbreviation;
-  final String? type;
-  final String? city;
-  final String? state;
-  final String? country;
-
-  Institution({
-    this.id,
-    this.name,
-    this.abbreviation,
-    this.type,
-    this.city,
-    this.state,
-    this.country,
-  });
-
-  factory Institution.fromJson(Map<String, dynamic> json) => Institution(
-    id: json["id"],
-    name: json["name"],
-    abbreviation: json["abbreviation"],
-    type: json["type"],
-    city: json["city"],
-    state: json["state"],
-    country: json["country"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "id": id,
-    "name": name,
-    "abbreviation": abbreviation,
-    "type": type,
-    "city": city,
-    "state": state,
-    "country": country,
   };
 }
 
