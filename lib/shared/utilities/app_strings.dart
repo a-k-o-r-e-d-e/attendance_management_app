229cc836990b59b439c40e0bc9f7f8e24b036672
habyaad <adeomotoshan@gmail.com>
2024-04-17 08:38:25 +0100

    feat: add new strings

--- lib/shared/utilities/app_strings.dart
+++ lib/shared/utilities/app_strings.dart
@@ -4,6 +4,13 @@ class AppStrings {
   static const String AUTH_TOKEN_KEY = "token";
   static const String APP_FIRST_RUN_KEY = 'check-first-run';
   static const String USER_JSON_KEY = "user-json";
+  static const String FCM_TOKEN_KEY = "fcm-token-key";
+  static const String NOTIFICATION_PERMISSION = "notification-key";
   static const String EXPIRATION_TIME_KEY = "token-expire";
   static const String ENABLE_GEOFENCING = "enable-geofencing";
+
+  static const String START_CLASS_EVENT = "start-class";
+  static const String EVENT_EXCEPTION = "exception";
+  static const String END_CLASS_EVENT = "end-class";
+  static const String END_CLASS_ACK = "end-class-acknowledged";
 }
