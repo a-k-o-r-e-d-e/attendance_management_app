aa23e1f3a24100d062fb6f5bfd5b71e374f7729c
habyaad <adeomotoshan@gmail.com>
2024-04-21 20:51:42 +0100

    feat: add new event strings

--- lib/shared/utilities/app_strings.dart
+++ lib/shared/utilities/app_strings.dart
@@ -10,7 +10,18 @@ class AppStrings {
   static const String ENABLE_GEOFENCING = "enable-geofencing";
 
   static const String START_CLASS_EVENT = "start-class";
+  static const String JOIN_CLASS_EVENT = "join-class";
+  static const String STUDENT_JOINED_CLASS_EVENT = "student-joined-class";
   static const String EVENT_EXCEPTION = "exception";
   static const String END_CLASS_EVENT = "end-class";
+  static const String CLASS_ENDED_EVENT = "class-ended";
   static const String END_CLASS_ACK = "end-class-acknowledged";
+
+  static const String TAKE_ATTENDANCE_EVENT = "take-attendance";
+  static const String HALT_ATTENDANCE_EVENT = "halt-attendance";
+  static const String MARK_ATTENDANCE_EVENT = "mark-attendance";
+  static const String ATTENDANCE_INITIATED_EVENT = "attendance-initiated";
+  static const String ATTENDANCE_STOPPED_EVENT = "attendance-stopped";
+
+  static const String STUDENT_MARKED_PRESENT_EVENT = "student-marked-present";
 }
